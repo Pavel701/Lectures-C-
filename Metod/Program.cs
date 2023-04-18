39716d1d22767a6cf4f199f790b9fda0d41521a5
flip701 <paulshipspb@gmail.com>
2023-04-18 14:54:56 +0300

    Изучили метод

---
+++ Metod/Program.cs
@@ -0,0 +1,22 @@
+﻿void Metod1(){
+
+    System.Console.WriteLine("Автор\t Шипунов П.Н.");
+}
+//Metod1();
+
+// void Metod2(string msg){
+
+// Console.WriteLine(msg);
+// }
+// Metod2(msg: "Text meseger");
+
+void Metod22(int msg, int count)
+{
+int i = 0;
+while(i < count)
+{
+    System.Console.WriteLine(msg);
+    i++;
+}
+}
+Metod22(msg: 152, count: 4);
